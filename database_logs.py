db6be652dff005d03d225563389531a4f6b3bfcc
Andriken Sonwane <andrikensonwane@gmail.com>
2019-08-15 20:51:30 +0530

    refactor: refactoring production code
    refactoring and neatly formatting the code for reusability and clarity

--- database_logs.py
+++ database_logs.py
@@ -2,46 +2,59 @@
 
 import psycopg2
 
-DBNAME = 'news'  # Database name
-"""Query for the question "What are the \
-    most popular three articles of all time?"""
-first_question_query = "select * from three_most_popular_articles;"
-"""Query for the question "Who are the most popular \
-    article authors of all time?"""
-second_question_query = "select * from popular_article_authors"
-"""Query for the question "On which days did \
-    more than 1% of requests lead to errors?"""
-third_question_query = "select * from days_with_more_than_1_percent_errors;"
+DBNAME = 'news' # Database name
+
+# Queries
+top_articles_query = "select * from three_most_popular_articles;"
+top_authors_query = "select * from popular_article_authors"
+top_error_days_query = "select * from days_with_more_than_1_percent_errors;"
 
 
-def query_logs():
-    """This function connects to database and runs queries \
-    to fetch the results."""
+def fetch_query(query):
+    """Connect to the database, query, fetch results,
+    close connection"""
+    results = None
     try:
-        # Connects to the database news
+        # Connects to the database news and grabs cursor
         db = psycopg2.connect(database=DBNAME)
         cursor = db.cursor()
-        # Executes the query and stores the each fetched results
-        cursor.execute(first_question_query)
-        top_articles = cursor.fetchall()
-        cursor.execute(second_question_query)
-        popular_authors = cursor.fetchall()
-        cursor.execute(third_question_query)
-        days_with_error = cursor.fetchall()
-
-        # Prints answer for first question
-        print("\n*** Most popular three articles ***")
-        for article in top_articles:
-            print(""""{0}" -- {1} views""".format(article[0], article[1]))
-        # Prints answer for second question
-        print("\n*** Most Popular Article Authors of all time ***")
-        for author in popular_authors:
-            print("{0} -- {1} views".format(author[0], author[1]))
-        # Prints answer for third question
-        print("\n*** Days on which more than 1% of requests led to errors ***")
-        for days in days_with_error:
-            print("{0} -- {1}".format(days[0], days[1]))
+        # Executes the query, fetch result and store into variable
+        cursor.execute(query)
+        results = cursor.fetchall()
     finally:
-        db.close()  # close the database
+        # Close database and return result
+        db.close()
+        return results
 
-query_logs()  # run the query_logs function
+
+def print_top_articles():
+    """Fetch top articles using helper function"""
+
+    top_articles = fetch_query(top_articles_query)
+    print("\n*** Most popular three articles ***")
+    for article in top_articles:
+        print(""""{0}" -- {1} views""".format(article[0], article[1]))
+
+
+def print_top_authors():
+    """Fetch top authors using helper function"""
+
+    popular_authors = fetch_query(top_authors_query)
+    print("\n*** Most Popular Article Authors of all time ***")
+    for author in popular_authors:
+        print("{0} -- {1} views".format(author[0], author[1]))
+
+
+def print_top_error_days():
+    """Fetch top error days using helper function"""
+
+    days_with_error = fetch_query(top_error_days_query)
+    print("\n*** Days on which more than 1% of requests led to errors ***")
+    for days in days_with_error:
+        print("{0} -- {1}".format(days[0], days[1]))
+
+
+if __name__ == '__main__':
+    print_top_articles()
+    print_top_authors()
+    print_top_error_days()
